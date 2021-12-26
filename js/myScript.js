9dacaa0e5f6b462f254478353e01f8df90fb925a
Awradt <96381504+Awradt@users.noreply.github.com>
2021-12-26 20:07:43 +0300

    26

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,34 @@
 function sendHello() {
-  alert("Hello " + );
+  alert("Hello ");
 }
+
+
+
+function sendHi() {
+  alert("Hi" + document.getElementById("hiName").value);
+  document.getElementById ("date").innerHTML = 'hi iam fatoma';
+  }
+  console.log ("aaaaaaaaaa");
+
+  let a =10;
+  var b =15;
+  const c =25;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log("a+b="+c);
+  console.log(a+"+"+b+"="+c);
+
+  var b=10;
+  
+  //قسمة
+  a /=2;
+  //comment for java
+
+  var kbt [10,15,20];
+  console.log (kbt);
+  console.log (kbt[0])
+  console.log (kbt[1])
+  console.log (kbt[2])
+  
+  
